45e3c641d2d83adb2f77077ef7cf589f66b4921b
LinqingZ <linqing201@gmail.com>
2024-04-19 01:43:39 -0400

    solved all problems

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,34 +1,64 @@
 const helloWorldRegex = (str) => {};
 
-const hasAVowel = (str) => {};
+// Q2
+const hasAVowel = (str) => {
+  return /[aeiou]/gi.test(str);
+};
 
 const hasCatsOrDogs = (str) => {};
 
-const hasVowelStart = (str) => {};
+// Q4
+const hasVowelStart = (str) => {
+  return /^[aeiou]/i.test(str);
+};
+
 
 const hasPunctuationEnd = (str) => {};
 
-const hasNothingOrDigits = (str) => {};
+// Q6
+const hasNothingOrDigits = (str) => {
+  // return /^$|^\d+$/.test(str);
+  return /^(|\d+)$/.test(str);
+};
 
 const hasNoFlippers = (str) => {};
 
-const isValidEmail = (str) => {};
+// Q8
+const isValidEmail = (str) => {
+  // only letter a-z, numbers (0-9), and periods(.) are allowed in email
+  return /^[\w\.]+@[\w\.]+\.[a-z]{2,4}$/i.test(str);
+};
 
 const isValidPhoneNumber = (str) => {};
 
-const matchAllNumbers = (str) => {};
+// Q10
+const matchAllNumbers = (str) => {
+  return str.match(/\d+/g) || [];
+};
 
 const matchAllNumbersAsNumbers = (str) => {};
 
-const matchAllWords = (str) => {};
+// Q12
+const matchAllWords = (str) => {
+  // return str.match(/\b\w+\b/g) || []; // wrong
+  return str.match(/[a-zA-Z']+/g) || [];
+};
 
 const replaceAllNumbers = (str) => {};
 
-const fixFileName = (str) => {};
+// Q14
+const fixFileName = (str) => {
+  return str.replace(/\s+/g, "_")
+};
 
 const nameRedacter = (str) => {};
 
-const camelToSnakeCase = (str) => {};
+// Q16
+const camelToSnakeCase = (str) => {
+  // return str.replace(/([A-Z])/g, match => `_${match.toLowerCase()}`); // best way
+  // return str.replace(/([A-Z])/g, match => `_${match.toLowerCase()}`).trim();
+  return str.replace(/([A-Z])/g, "_$1").toLowerCase();
+};
 
 module.exports = {
   helloWorldRegex,
